fd924a8fab0a9adb43ba4daa924b8815caa63b84
roman <roman.mueller@swisslife.ch>
2020-04-25 16:51:38 +0200

    cleaned out apikeys.cs

--- src/TravelMonkey/ApiKeys.cs
+++ src/TravelMonkey/ApiKeys.cs
@@ -2,13 +2,17 @@
 {
 	public static class ApiKeys
 	{
-        #warning You need to set up your API keys.
-		public static string ComputerVisionApiKey = "";
+        public static string ComputerVisionApiKey = "";
 		public static string TranslationsApiKey = "";
 		public static string BingImageSearch = "";
+		public static string SpeechApiKey = "";
 
 		// Change this to the Azure Region you are using
-		public static string ComputerVisionEndpoint = "https://westeurope.api.cognitive.microsoft.com/";
-		public static string TranslationsEndpoint = "https://api.cognitive.microsofttranslator.com/";
+		public static string Region = "";
+		public static string SpeechAuthEndpoint = "";
+		public static string SpeechEndpoint = "";
+		public static string ComputerVisionEndpoint = "";
+		public static string TranslationsEndpoint = "";
+		public static string VoicesEndpoint = "";
 	}
 }
